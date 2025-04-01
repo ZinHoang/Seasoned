56bc8f2202c4c4fb9a344aa57bf97f6693b02e74
Zin Hoang <hoang.mantue@gmail.com>
2025-04-01 13:11:59 +0700

    Update the endpoint to suit v2 of Edamam API

--- client/apis/recipes.js
+++ client/apis/recipes.js
@@ -3,9 +3,9 @@ import request from 'superagent'
 export async function getRecipes(userInput) {
   try {
     const res = await request.get(
-      `https://api.edamam.com/search?q=${userInput}&app_id=${process.env.APP_ID}&app_key=${process.env.EDAMAM_API_KEY}&from=0&to=6`
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${userInput}&app_id=${process.env.APP_ID}&app_key=${process.env.EDAMAM_API_KEY}&from=0&to=6`
     )
-    const result = res.body.hits
+    const result = res.body.hits.slice(0, 15)
     return result
   } catch (err) {
     console.log('Error getting recipes from Edamam API. Error: ' + err)
